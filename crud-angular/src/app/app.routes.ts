63adf87f0fc6d27891f390e4eac5689e4a50be48
Luís Ledebour <luis.ledebour@gmail.com>
2026-03-17 15:01:22 -0300

    reating routes and modules with lazy loading

--- crud-angular/src/app/app.routes.ts
+++ crud-angular/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',redirectTo:'courses', pathMatch:'full'},
+  {path:'courses',
+    loadChildren:()=>import('./courses/courses-module').then(m=>m.CoursesModule)
+  }
+];
