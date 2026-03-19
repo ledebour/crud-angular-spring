4773c30531c76ea79dadc7a5673c176321d111e3
Luís Ledebour <luis.ledebour@gmail.com>
2026-03-18 21:32:02 -0300

    move imports to module of component

--- crud-angular/src/app/courses/courses-module.ts
+++ crud-angular/src/app/courses/courses-module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { CoursesComponent } from './courses/courses';
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { CoursesRoutingModule } from './courses-routing-module';
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, CoursesRoutingModule],
+  declarations: [CoursesComponent],
+  imports: [CommonModule, CoursesRoutingModule, MatTableModule, MatCardModule, MatToolbarModule ],
 })
 export class CoursesModule {}
--- crud-angular/src/app/courses/courses/courses.ts
+++ crud-angular/src/app/courses/courses/courses.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
-import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-courses',
-  imports: [MatTableModule  ],
   templateUrl: './courses.html',
   styleUrl: './courses.scss',
+  standalone: false,
 })
 export class CoursesComponent implements OnInit {
 
