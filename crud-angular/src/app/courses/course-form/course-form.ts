0340cfe4b61a1d09c144f92bd06f2dc61034c978
Luís Ledebour <luis.ledebour@gmail.com>
2026-03-21 20:19:57 -0300

    create http post to save data

--- crud-angular/src/app/courses/course-form/course-form.ts
+++ crud-angular/src/app/courses/course-form/course-form.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from "../../shared/shared-module";
 import { Courses } from '../services/courses';
 import { MatSnackBar  } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-course-form',
@@ -17,7 +18,8 @@ export class CourseForm implements OnInit {
   form:FormGroup;
   constructor(private readonly formBuilder: FormBuilder,
     private readonly service: Courses,
-    private readonly _snackBar: MatSnackBar
+    private readonly _snackBar: MatSnackBar,
+    private readonly location: Location
   ) {
     this.form = this.formBuilder.group({
       name: [''],
@@ -32,15 +34,21 @@ export class CourseForm implements OnInit {
   onSubmit(): void {
     console.log(this.form.value);
     this.service.save(this.form.value)
-    .subscribe(result=>console.log(result),error=>this.onError());
+    .subscribe(result=>this.onSuccess(),error=>this.onError());
   }
 
   onCancel(): void {
     console.log('onCancel');
+    this.location.back();
   }
 
   private onError(){
     this._snackBar.open("Erro ao salvar curso",'',{duration:5000});
+  }
+
+  private onSuccess(){
+    this._snackBar.open("Curso salvo com sucesso!",'',{duration:5000});
+    this.onCancel();
   }
 
 }
--- crud-spring/src/main/java/com/ledebour/crud_spring/controller/CourseController.java
+++ crud-spring/src/main/java/com/ledebour/crud_spring/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ledebour.crud_spring.model.Course;
@@ -11,7 +12,11 @@ import com.ledebour.crud_spring.repository.CourseRepository;
 
 import lombok.AllArgsConstructor;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -25,4 +30,11 @@ public class CourseController {
     public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
     }   
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
+    }
+    
 }
