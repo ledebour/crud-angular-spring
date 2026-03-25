46f420848cb2c528d94058f7bcac9c55eadd0f4e
Luís Ledebour <luis.ledebour@gmail.com>
2026-03-25 00:17:07 -0300

    add validation in APIs and database with spring boot starter validation

--- crud-spring/src/main/java/com/ledebour/crud_spring/model/Course.java
+++ crud-spring/src/main/java/com/ledebour/crud_spring/model/Course.java
@@ -1,5 +1,7 @@
 package com.ledebour.crud_spring.model;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -7,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
@@ -18,9 +21,15 @@ public class Course {
     @JsonProperty("_id")
     private Long id;
 
-    @Column(length = 200, nullable = false)
+    @NotBlank
+    @NotNull
+    @Length(min=5, max=100)
+    @Column(length = 100, nullable = false)
     private String name;
 
+    @NotNull
+    @Length(max=10)
+    @Pattern(regexp = "Back-end|Front-end")
     @Column(length = 10, nullable = false)
     private String category;
 }
